e5266776a9c8e0e5828918a64e9433709e9ae07c
kristjanjansen <kristjan.jansen@gmail.com>
2016-12-19 11:53:10 +0200

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+## Install and development
+
+```
+yarn
+npm run dev
+```
+
+## Production
+
+```
+npm install
+npm run build
+npm run start
+```
---
+++ src/pages/dash/dash.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <site
+            class="margin-bottom-md"
+            v-for="site in sites"
+            :site="site"
+        >
+        </site>
+    </div>
+</template>
+
+<script>
+
+    import yaml from 'js-yaml'
+    import site from '../../components/site/site.vue'
+
+    export default {
+        components: { site },
+        data: function() {
+            return {
+                sites: []
+            }
+        },
+        created() {
+            this.$http.get('./data/dash.yaml')
+                .then(res => {
+                    this.sites = yaml.safeLoad(res.body)
+                })
+        }
+    }
+
+</script>
